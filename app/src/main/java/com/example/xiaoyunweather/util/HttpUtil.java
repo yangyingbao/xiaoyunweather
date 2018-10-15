607195999fc4a8093090eb7c406e587677727d04
yangyingbao <yangyingbao1234@163.com>
2018-10-15 15:31:18 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/xiaoyunweather/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.example.xiaoyunweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+    /**
+     * 网络服务工具类
+     * @param address
+     * @param callback
+     */
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
